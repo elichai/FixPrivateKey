53f62021d7f7b412a66dd3a896bdf33384b7d2f1
Elichai Turkel <elichai.turkel@gmail.com>
2014-11-25 15:51:50 +0200

    Added options to remove or add characters if needed.

--- FixAndCheckPrivateKey.py
+++ FixAndCheckPrivateKey.py
@@ -11,6 +11,17 @@ possible = possible.translate(None, 'OIl')
 result = []
 
 
+def query_yes_no(question):
+    valid = {"yes": True, "y": True, "no": False, "n": False}
+    while True:
+        print (question + " [y/n]"),
+        choice = raw_input().lower()
+        if choice in valid:
+            return valid[choice]
+        else:
+            print "Please respond with 'yes' or 'no' (or 'y' or 'n').\n"
+
+
 def check(key):
     key_decoded = bitcoin.base58.decode(key)
     check = key_decoded[-4:]
@@ -22,17 +33,32 @@ def check(key):
 
 
 def change_letter(address, letters, start):
-    if 52 < len(address) < 51:
+    if len(address) < 50 or len(address) > 52:
         return 'The address is too long\short'
+
+    if len(address) == 52:
+        if address[1] is 'L' or 'K':
+            pass
+        elif not query_yes_no("Your privkey is 1 char too long, do you want to remove the last one?"):
+            return "Canceled by user"
+        else:
+            address = address[:-1]
+        print 'yep'
+
+    if len(address) == 50:
+        if not query_yes_no("Your privkey is 1 char short, do you want to try generate the last one?"):
+            return "Canceled by user"
+        address += '1'
+
     if check(address):
         return address
+
     address_list = list(address)
     for i in range(start, len(address_list)):
         for c in possible:
             address_temp = list(address_list)
             address_temp[i] = c
             address_end = "".join(address_temp)
-            # print address_end + " " + str(letters)
             if check(address_end):
                 if args.results == 1:
                     return address_end
